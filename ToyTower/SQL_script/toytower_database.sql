b5657d1bef8328511b8b38678b748c7828460147
Mikepin23 <137085441+Mikepin23@users.noreply.github.com>
2023-10-23 13:08:06 -0400

    Website / Beginning of SQL script push

---
+++ ToyTower/SQL_script/toytower_database.sql
@@ -0,0 +1,26 @@
+/*
+CREATE DATABASE toytower
+;
+*/
+
+USE toytower
+;
+
+CREATE TABLE Users -- Create account
+(
+	UserID int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    Username varchar(50) NOT NULL,
+    UserPass varchar(50) NOT NULL,
+    UserFN varchar(50) NOT NULL,
+    UserLN varchar(50) NOT NULL,
+    UserEmail varchar(150) NOT NULL,
+    Phone varchar(10) NOT NULL,
+    PostalCode varchar(7) NOT NULL
+)
+;
+
+/*
+CREATE TABLE Login
+;
+*/
+
