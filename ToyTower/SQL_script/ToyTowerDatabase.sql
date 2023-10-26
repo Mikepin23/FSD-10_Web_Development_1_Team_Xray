1ec04363c257d80bac59ef5a16dec9e614a3e1f4
ZacharyLyonMcRae <135399195+ZacharyLyonMcRae@users.noreply.github.com>
2023-10-26 13:40:35 -0400

    database added, and carousel added.
    these functions are added now.

---
+++ ToyTower/SQL_script/ToyTowerDatabase.sql
@@ -0,0 +1,59 @@
+CREATE TABLE Customers (
+    CustomerID INT AUTO_INCREMENT,
+    Address VARCHAR(255),
+    PostalCode VARCHAR(10),
+    `Name` VARCHAR(255),
+    TelephoneNumber VARCHAR(15),
+    PRIMARY KEY (CustomerID)
+);
+
+CREATE TABLE Brands (
+    BrandID INT AUTO_INCREMENT,
+    Name VARCHAR(255),
+    PRIMARY KEY (BrandID)
+);
+
+CREATE TABLE AgeRanges (
+    AgeRangeID INT AUTO_INCREMENT,
+    `Range` VARCHAR(255),
+    PRIMARY KEY (AgeRangeID)
+);
+
+CREATE TABLE Categories (
+    CategoryID INT AUTO_INCREMENT,
+    `Name` VARCHAR(255),
+    PRIMARY KEY (CategoryID)
+);
+
+CREATE TABLE Products (
+    ProductID INT,
+    `Name` VARCHAR(255),
+    BrandID INT,
+    AgeRange INT,
+    CategoryID INT,
+    PRIMARY KEY (ProductID),
+    FOREIGN KEY (BrandID) REFERENCES Brands(BrandID),
+    FOREIGN KEY (AgeRange) REFERENCES AgeRanges(AgeRangeID),
+    FOREIGN KEY (CategoryID) REFERENCES Categories(CategoryID)
+);
+
+CREATE TABLE Transactions (
+    OrderID INT,
+    CustomerID INT,
+    ProductID INT,
+    Total DECIMAL(10, 2),
+    PRIMARY KEY (OrderID),
+    FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID),
+    FOREIGN KEY (ProductID) REFERENCES Products(ProductID)
+);
+
+CREATE TABLE OrderItems (
+    OrderItemID INT,
+    OrderID INT,
+    ProductID INT,
+    Quantity INT,
+    Subtotal DECIMAL(10, 2),
+    PRIMARY KEY (OrderItemID),
+    FOREIGN KEY (OrderID) REFERENCES Transactions(OrderID),
+    FOREIGN KEY (ProductID) REFERENCES Products(ProductID)
+);
