4f854ccd4f2f0c9244b020b642c723e089a143b0
Mikepin23 <137085441+Mikepin23@users.noreply.github.com>
2023-10-29 20:55:01 -0400

    Populated some tables in the database, added fatfree using composer, worked on the routes for the index.php

---
+++ ToyTower/SQL_script/populate_toytower_database.sql
@@ -0,0 +1,41 @@
+INSERT INTO Brands (BrandName)
+VALUES 
+('Lego'),
+('Barbie'),
+('Marvel'),
+('DC'),
+('Disney'),
+('Pixar'),
+('Paw Patrol'),
+('Hot Wheels'),
+('Jurrasic World'),
+('Sonic'),
+('Play-Doh'),
+('Peppa Pig')
+;
+
+INSERT INTO AgeRanges (`Range`)
+VALUES 
+('0 - 2 years'),
+('3 - 4 years'),
+('5 - 7 years'),
+('8 - 10 years'),
+('11 - 12 years'),
+('13+ years')
+;
+
+INSERT INTO Categories (CategoryName)
+VALUES 
+('Action Figures'),
+('Learning'),
+('Kids Boods'),
+('Activity Games'),
+('Sports'),
+('Board Games'),
+('Collectibles'),
+('Puzzles'),
+('Vehicles'),
+('Stuffed Animals'),
+('Arts and Crafts'),
+('Toddlers')
+;
